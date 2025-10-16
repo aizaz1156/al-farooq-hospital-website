6a8e1b53f6d6e6c8495e5c2c92efb99ae052aaad
Aizaz Ahmad <aizaz1156@gmail.com>
2025-10-16 09:58:38 +0500

    Add files via upload

---
+++ Al Farooq Hospital Source Code.py
@@ -0,0 +1,287 @@
+SOURCE CODE 
+<body> 
+<!-- this is our logo plus h1 tag --> 
+<div style="width:100%; height: 250px; background-color: white; "> 
+<div style="height: 250PX; width: 25%; float:left; text-align: center;"> 
+<img src="index.png"> 
+</div> 
+<div style=" height: 250PX; width :74%; float: left;"> 
+<h1 style="text-align:center; margin-top:5%; text-"> 
+THE AL-FAROOQ MEDICAL 
+COLLEGE AND TREATMENT CENTER PAKISTAN</h1> 
+</div> 
+</div> 
+<!-- this is our menu to hold links --> 
+<div style="width:100%x; height: 66px; background-color: lightgrey; " > 
+<ol style =" width : 80%; height :65px; float: left; margin-left: -32px; "> 
+<a href="home.html"><li>HOME</li></a> 
+<a href="registration.html"><li>REGISTRATION</li></a> 
+<a href="contact.html"><li>CONTACT</li></a> 
+<a href="help.html"><li>SECVICES</li></a> 
+</ol> 
+<div style="width: 100%; height:830px; border: 5px solid lightgrey; "> 
+<div style="width: 30%; height: 765px; float: left; border: 5px solid 
+gray; margin-top: 3%;"> 
+<img src=" index.png" height="200px" width="60%" 
+align="center" style="margin-left: 80px; margin-top: 3%;"> 
+<h4 style=" margin-top: 50px; text-transform: 
+uppercase; font-family: monospace; "> 
+<p> 
+The hospital was established in 
+March 2003. It first started off as New Mvita Hospital and then later on in 2009 was renamed and 
+registered as Al Farooq Hospital. 
+Alfarooq Hospital is a medical 
+institution dedicated to providing quality patient care with unrelenting attention to clinical 
+excellence, patient 
+safety and an unparalleled passion 
+and commitment to assure the very best healthcare for those we serve. 
+</p> 
+</h4> 
+</div> 
+<div style="width: 68%; height: 750px; float:right; margin-top: 4%; 
+border: 1px solid gray;"> 
+<div style="float: left; width: 100%; "> 
+<h2 
+style="text-transform: 
+uppercase; 
+margin-left: 1%; color: darkblue; margin-top: 3%; margin-bottom: 4%;"> AL-FAROOQ 
+HOSPITAL AIMs</h2> 
+<div style=" width: 45%; height: 300px ; margin-left: 2% ; float: left; "> 
+<img src="a.png" width="100%" height="100%"> 
+</div> 
+<div style=" width: 45%; height: 300px; margin-left: 2%; float: left;"> 
+<img src="aa.png" width="100%" height="100%"> 
+</div> 
+</div> 
+<div style="float: left; width: 100%; margin-top: 5%; "> 
+<div style=" width: 45%; height: 300px ; margin-left: 2% ; float: left;"> 
+<img src="ddddd.png" width="100%" height="100%"> 
+</div> 
+<div style=" width: 45%; height: 300px; margin-left: 2%; float: left;"> 
+<img src="w.png" width="100%" height="100%"> 
+</div> 
+</div> 
+</div> 
+</div> 
+<div style="width: 100%; height: 220px; background-color: white; color: brown; margin-top: 
+3%;"> 
+<h1 style="text-align: center; "> 
+AL-FAROOQ HOSPITAL AND MEDICAL COLLEGE 
+</h1> 
+<h3 style="text-align: center;"> 
+WHATS APP NO: 0303-9009306 
+</h3> 
+<h3 style="text-align: center;"> 
+CONTACT NO: 0342-9391523 
+</h3> 
+<h4 style=" text-align: center;"> 
+NEAR FAROOQ SHOPPING MALL KHYBER PLAZA 
+SAKHAKOT TEHSIL DARGAI DISTRICT MALAKAND 
+</h4> 
+</div> 
+</div> 
+</div> 
+</body> 
+</html> 
+REGISTRATION PAGE / ONLINE APPOINTMENT: 
+This page is especially for patients whose are interested to book an appointment 
+from a doctor at AL-FAROOQ hospital. Also at time of emergency any one can book an 
+appointment. 
+SOURCE CODE: 
+<body> 
+<!-- this is our logo plus h1 tag --> 
+<div style="width:100%; height: 250px; background-color: white; "> 
+<div style="height: 250PX; width: 25%; float:left; text-align: center;"> 
+<img src="index.png"> 
+</div> 
+<div style=" height: 250PX; width :74%; float: left;"> 
+<h1 style="text-align:center; margin-top:5%; text-"> 
+THE AL-FAROOQ MEDICAL 
+COLLEGE AND TREATMENT CENTER PAKISTAN</h1> 
+</div> 
+</div> 
+<!-- this is our menu to hold links --> 
+<div style="width:100%x; height: 66px; background-color: lightgrey; " > 
+<ol style =" width : 80%; height :65px; float: left; margin-left: -32px; "> 
+<a href="home.html"><li>HOME</li></a> 
+<a href="registration.html"><li>REGISTRATION</li></a> 
+<a href="contact.html"><li>CONTACT</li></a> 
+<a href="help.html"><li>SECVICES</li></a> 
+</ol> 
+<div style="width: 80%; height: 610px; margin-left: 10%; background-color: 
+lightgrey; margin-top: 3%;"> 
+<div style="width: 80%; height: 570px; margin-left: 10%; margin-top: 
+3%;"> 
+<form method="POST" > 
+<div style="margin-left: 10%;" > 
+<p>Name</p> 
+<input type="input" name="" style="width: 80%; 
+height: 35px ; margin: center; font-style: oblique; font-size: 18px;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> father Name</p> 
+<input type="input" name="" style="width: 80%; 
+height: 35px ; margin: center;font-style: oblique; font-size: 18px;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> CNIC</p> 
+<input type="input" name="" style="width: 80%; 
+height: 35px ; margin: center; font-style: oblique; font-size: 18px;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> INFECTION</p> 
+<input type="input" name="" style="width: 80%; 
+height: 35px ; margin: center; font-style: oblique; font-size: 18px;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> CONTACT NUMBER</p> 
+<input type="input" name="" style="width: 80%; 
+height: 35px ; margin: center; font-style: oblique; font-size: 18px;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> ADDRESS</p> 
+<input type="input" name="" style="width: 80%; 
+height: 35px ; margin: center; font-style: oblique; font-size: 18px;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> </p> 
+<input type="SUBMIT" VALUE="SEND 
+RECORD" name="" style="width: 40%; height: 60px ; margin-left: 20%; background-color: 
+lightgreen; font-size: 20px;" > 
+</div> 
+</form> 
+</div> 
+</div> 
+</body> 
+</html> 
+COMPLAIN CENTER OR SUGGESTION: -FAROOQ hospital. 
+This page allows clients to submit their complaints and suggestions for AL 
+SOURCE CODE: 
+<body> 
+<!-- this is our logo plus h1 tag --> 
+<div style="width:100%; height: 250px; background-color: white; "> 
+<div style="height: 250PX; width: 25%; float:left; text-align: center;"> 
+<img src="index.png"> 
+</div> 
+<div style=" height: 250PX; width :74%; float: left;"> 
+<h1 style="text-align:center; margin-top:5%; text-"> 
+THE AL-FAROOQ MEDICAL 
+COLLEGE AND TREATMENT CENTER PAKISTAN</h1> 
+</div> 
+</div> 
+<!-- this is our menu to hold links --> 
+<div style="width:100%x; height: 66px; background-color: lightgrey; " > 
+<ol style =" width : 80%; height :65px; float: left; margin-left: -32px; "> 
+<a href="home.html"><li>HOME</li></a> 
+<a href="registration.html"><li>REGISTRATION</li></a> 
+<a href="contact.html"><li>CONTACT</li></a> 
+<a href="help.html"><li>SECVICES</li></a> 
+</ol> 
+<div style="width: 80%; height: 600px; border: 1px solid grey; margin-left: 10%; background- 
+color: lightgrey; margin-top: 3%;"> 
+<div style="width: 80%; height: 510px; margin-left: 10%; margin-top: 5%;"> 
+<form method="POST" > 
+<div style="margin-left: 10%;" > 
+<p>Name</p> 
+<input type="input" name="" style="width: 80%; height: 44px ; margin: center;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> CONTACT NUMBER</p> 
+<input type="input" name="" style="width: 80%; height: 44px ; margin: center;" > 
+</div> 
+<div style="margin-left: 10%;" > 
+Gmail</p> 
+<input type="input" name="" style="width: 80%; height: 44px ; margin: center;" > 
+</div> 
+<div style="margin-left: 10%;" ><p> SUGGESTION</p> 
+<textarea cols="67" rows="5"></textarea> 
+</div> 
+<div style="margin-left: 10%;" > 
+<p> 
+<input type="SUBMIT" VALUE="SEND RECORD" name="" style="width: 40%; height: 
+60px ; margin-left: 20%; background-color: lightgreen; font-size: 20px;" > 
+</div> 
+</form> 
+</div> 
+</div> 
+<div style="width: 100%; height: 220px; background-color: white; color: brown; margin-top: 
+3%;"> 
+<h1 style="text-align: center; "> 
+AL-FAROOQ HOSPITAL AND MEDICAL COLLEGE 
+</h1> 
+<h3 style="text-align: center;"> 
+WHATS APP NO: 0303-9009306 
+</h3> 
+<h3 style="text-align: center;"> 
+CONTACT NO: 0342-9391523 
+</h3> 
+<h4 style=" text-align: center;"> 
+NEAR FAROOQ SHOPPING MALL KHYBER PLAZA SAKHAKOT TEHSIL DARGAI 
+DISTRICT MALAKAND</h4> 
+</div> 
+</body> 
+OUR SERVICES OR TREATMENT: 
+This Page is represent that which treatment are available at AL-FAROOQ 
+hospital and which can be treated and can recover patients. 
+<body> 
+<!-- this is our logo plus h1 tag --> 
+<div style="width:100%; height: 250px; background-color: white; "> 
+<div style="height: 250PX; width: 25%; float:left; text-align: center;"> 
+<img src="index.png"> 
+</div> 
+<div style=" height: 250PX; width :74%; float: left;"> 
+<h1 style="text-align:center; margin-top:5%; text-"> 
+THE AL-FAROOQ MEDICAL 
+COLLEGE AND TREATMENT CENTER PAKISTAN</h1> 
+</div> 
+</div> 
+<!-- this is our menu to hold links --> 
+<div style="width:100%x; height: 66px; background-color: lightgrey; " > 
+<ol style =" width : 80%; height :65px; float: left; margin-left: -32px; "> 
+<a href="home.html"><li>HOME</li></a> 
+<a href="registration.html"><li>REGISTRATION</li></a> 
+<a href="contact.html"><li>CONTACT</li></a> 
+<a href="help.html"><li>SECVICES</li></a> 
+</ol> 
+</div> 
+<div style="width: 80%; height: 830px; margin-left: 10%; border: 1px solid gray;"> 
+<div style="float: left; width: 100%; "> 
+<h2 style="text-transform: uppercase; margin-left: 1%; text-decoration-line: underline; margin- 
+top: 3%; margin-bottom: 4%;"> AL-FAROOQ HOSPITAL PROVIDE THE FOLLOWING 
+SERVICES</h2> 
+<div style=" width: 45%; height: 300px ; margin-left: 2% ; float: left; "> 
+<h2>stomach</h2> 
+<img src="stomach.png" width="100%" height="100%"> 
+</div> 
+<div style=" width: 45%; height: 300px; margin-left: 2%; float: left;"> 
+<h2>Kidney stone</h2> 
+<img src="kidney.png" width="100%" height="100%"> 
+</div> 
+</div> 
+<div style="float: left; width: 100%; margin-top: 5%; "> 
+<div style=" width: 45%; height: 300px ; margin-left: 2% ; float: left;"> 
+<h2>Intastine </h2> 
+<img src="intastine.png" width="100%" height="100%"> 
+</div> 
+<div style=" width: 45%; height: 300px; margin-left: 2%; float: left;"> 
+<h2>Cordolgy</h2> 
+<img src="heart.png" width="100%" height="100%"> 
+</div> 
+</div> 
+</div> 
+<div style="width: 100%; height: 220px; background-color: white; color: brown; margin-top: 
+5%;"> 
+<h1 style="text-align: center; "> 
+AL-FAROOQ HOSPITAL AND MEDICAL COLLEGE 
+</h1> 
+<h3 style="text-align: center;"> 
+WHATS APP NO: 0303-9009306 </h3> 
+<h3 style="text-align: center ;"> 
+CONTACT NO: 0342-9391523 
+</h3> 
+<h4 style=" text-align: center ;"> 
+NEAR FAROOQ SHOPPING MALL KHYBER PLAZA SAKHAKOT TEHSIL DARGAI 
+DISTRICT MALAKAND 
+</h4> 
+</div> 
+</body>
---
+++ README.md
@@ -0,0 +1,140 @@
+THE AL-FAROOQ MEDICAL HOSPITAL 
+ 
+Tools used in project: 
+HTML 
+CSS 
+
+Text editors: 
+Note paid 
+Note paid ++ 
+Dremveawer 
+Text sublime 3 
+
+Web browsers: 
+Internet explorer 
+Fire fox 
+Microsoft edge 
+Google chrome 
+Opera 
+
+HTML: 
+Stands for Hypertext Markup Language. It is a standard markup language for web page 
+creation. It allows the creation and structure of sections, paragraphs, and links using HTML 
+elements (the building blocks of a web page) such as tags and attributes. 
+HTML has a lot of use cases, namely: 
+Web development. Developers use HTML code to design how a browser displays web 
+page elements, such as text, hyperlinks, and media files. 
+Internet navigation. Users can easily navigate and insert links between related pages and 
+websites as HTML is heavily used to embed hyperlinks. 
+Web documentation. HTML makes it possible to organize and format documents, similarly 
+to Microsoft Word. 
+It also worth noting that HTML is not considered a programming language as it can’t create 
+dynamic functionality. It is now considered an official web standard. The World Wide Web 
+Consortium (W3C) maintains and develops HTML specifications, along with providing regular 
+updates 
+The average website includes several different HTML pages. For instance, a home page, an about 
+page, and a contact page would all have separate HTML files. 
+HTML documents are files that end with a .html or .htm extension. A web browser reads the 
+HTML file and renders its content so that internet users can view it. 
+All HTML pages have a series of HTML elements, consisting of a set of tags and attributes. HTML 
+elements are the building blocks of a web page. A tag tells the web browser where an element 
+begins and ends, whereas an attribute describes the characteristics of an element. 
+The three main parts of an element are: 
+Opening tag – used to state where an element starts to take effect. The tag is wrapped with 
+opening and closing angle brackets. For example, use the start tag <p> to create a 
+paragraph. 
+Content – this is the output that other users see. 
+Closing tag – the same as the opening tag, but with a forward slash before the element 
+name. For example, </p> to end a paragraph. 
+Most Used HTML Tags and HTML Elements 
+Currently, there are 142 HTML tags available that allow for the creation of various elements. Even 
+though modern browsers no longer support some of these tags, learning all the different elements 
+available is still beneficial. 
+This section will discuss the most-used HTML tags and two main elements – block-level elements 
+and inline elements. 
+
+Block-Level Elements 
+A block-level element takes up the entire width of a page. It always starts a new line in the 
+document. For example, a heading element will be in a separate line from a paragraph element. 
+
+Every HTML page uses these three tags: 
+<html> tag is the root element that defines the whole HTML document. 
+<head> tag holds meta information such as the page’s title and charset. 
+<body> tag encloses all the content that appears on the page. 
+<html> 
+<head> 
+<!-- META INFORMATION --> 
+</head> 
+<body> 
+<!-- PAGE CONTENT --> 
+</body> 
+</html> 
+
+Other popular block-level tags include: 
+ Heading tags – these range from <h1> to <h6>, where heading h1 is largest in size, getting 
+smaller as they move up to h6. 
+ Paragraph tags – are all enclosed by using the <p> tag. 
+ List tags – have different variations. Use the <ol> tag for an ordered list, and use <ul> for 
+an unordered list. Then, enclose individual list items using the <li> tag. 
+
+Inline Elements 
+An inline element formats the inner content of block-level elements, such as adding links and 
+emphasized strings. Inline elements are most commonly used to format text without breaking the 
+flow of the content. 
+For example, a <strong> tag would render an element in bold, whereas the <em> tag would show it 
+in italics. Hyperlinks are also inline elements that use an <a> tag and an href attribute to indicate 
+the link’s destination: 
+<a href="https://example.com/">Click me!</a> 
+HTML Evolution – What Differs Between HTML and HTML5? 
+The first version of HTML consisted of 18 tags. Since then, each new version came with new tags 
+and attributes added to the markup. The most significant upgrade of the language so far was the 
+introduction of HTML5 in 2014. 
+The main difference between HTML and HTML5 is that HTML5 supports new kinds of form 
+controls. HTML5 also introduced several semantic tags that clearly describe the content, such as 
+<article>, <header>, and <footer>. 
+
+Cascading Style Sheets 
+CSS, is a simply designed language intended to simplify the process of making web pages 
+presentable. CSS allows you to apply styles to web pages. More importantly, CSS enables you to 
+do this independent of the HTML that makes up each web page. It describes how a webpage 
+should look: it prescribes colors, fonts, spacing, and much more. In short, you can make your 
+website look however you want. CSS lets developers and designers define how it behaves, 
+including how elements are positioned in the browser. 
+
+Internal, External and Inline CSS Styles 
+We’ll go over each style briefly, for an in-depth explanation of each method, there will be a link 
+below the overview. Let’s start by talking about the Internal style. CSS styles done this way are 
+loaded each time a website is refreshed, which may increase loading time. Additionally, you won’t 
+be able to use the same CSS style on multiple pages as it’s contained within a single page. 
+However, this also comes with benefits. Having everything on one page makes it easier to share 
+the template for a preview. 
+The External method might be the most convenient one. Everything is done externally on a .css 
+file. This means you can do all the styling on a separate file and apply the CSS to any page you 
+want. The External style might also improve loading times. 
+Lastly, we will talk about the Inline style of CSS. Inline works with specific elements that have the 
+<style> tag. Each component has to be stylized, so it might not be the best or fastest way to handle 
+CSS. But it can come in handy. For example, if you want to change a single element, quickly 
+preview changes, or maybe you don’t have access to the CSS files. 
+
+External CSS: 
+External CSS contains separate CSS file which contains only style property with the help 
+of tag attributes (For example class, id, heading, … etc). CSS property written in a separate file 
+with .css extension and should be linked to the HTML document using link tag. This means that for 
+each element, style can be set only once and that will be applied across web pages. 
+The AL-FAROOQ hospital, an institution that is built, staffed, and equipped for the diagnosis of 
+disease; for the treatment, both medical and surgical, of the sick and the injured; and for their 
+housing during this process. The modern hospital also often serves as a centre for investigation 
+and for teaching. 
+To better serve the wide-ranging needs of the community, the modern hospital has often 
+developed outpatient facilities, as well as emergency, psychiatric, and rehabilitation services. In 
+addition, “bed less hospitals” provide strictly ambulatory (outpatient) care and day surgery. 
+Patients arrive at the facility for short appointments. They may also stay for treatment in surgical 
+or medical units for part of a day or for a full day, after which they are discharged for follow-up 
+by a primary care health provider. 
+Hospitals have long existed in most countries. Developing countries, which contain a large 
+proportion of the world’s population, generally do not have enough hospitals, equipment, and 
+trained staff to handle the volume of persons who need care. Thus, people in these countries do 
+not always receive the benefits of modern medicine, public health measures, or hospital care, and 
+they generally have lower life expectancies. 
+This project will help people to get appoints form home in case emergency.it will be also for help 
+for people by checking online which treatments are available here.
